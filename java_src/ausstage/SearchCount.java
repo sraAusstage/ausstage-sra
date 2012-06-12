115f4b5cd2c4d2930e7cab1c1c2e98328506dd24
sirbrad <sirbrad@hotmail.com>
2012-06-12 05:20:25 +0000

    Search Count java for initial search page

---
+++ java_src/ausstage/SearchCount.java
@@ -0,0 +1,313 @@
+/***************************************************
+
+Company: SRA
+ Author: Brad Williams
+Project: Ausstage
+
+   File: SearchCount.java
+
+Purpose: Provides count of search results for initial page. 
+
+***************************************************/
+
+package ausstage;
+
+import admin.Common;
+import ausstage.Database;
+import java.sql.*;
+import sun.jdbc.rowset.*;
+import java.lang.Character;
+import java.util.Vector;
+import java.util.GregorianCalendar;
+
+  public class SearchCount {
+	private Database m_db;
+  	private admin.Common common = new admin.Common();
+  	
+
+
+  	public SearchCount(Database p_db) {
+	  m_db = p_db;
+  	}
+  
+  	public SearchCount(Database p_db,java.sql.Connection p_connection) {
+	  m_db = p_db;
+  	}
+  	
+  	/*******************
+    GET FUNCTIONS
+    *******************/
+  	public CachedRowSet getEventCount(){
+  	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM events WHERE "
+  							+"LOWER(event_name) LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getEventCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);
+  	}
+  	
+  	
+  	public CachedRowSet getContributorCount(){
+  	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM contributor WHERE "
+  							+"CONCAT(LOWER(first_name),' ',LOWER(last_name)) LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getContributorCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);
+  	}
+
+  	
+  	
+  	public CachedRowSet getVenueCount(){
+  	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM venue WHERE "
+  							+"venue_name LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getVenueCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);
+  		
+  	}
+  	
+  	public CachedRowSet getOrganisationCount(){
+  	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM organisation WHERE "
+  							+"name LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getOrganisationCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);
+  	}
+  	
+  	
+  	public CachedRowSet getGenreCount(){
+ 	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM secgenrepreferred WHERE "
+  							+"preferredterm LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getGenreCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);	
+  	}
+  	
+  	public CachedRowSet getFunctionCount(){
+ 	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM contributorfunctpreferred WHERE "
+  							+"preferredterm LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getFunctionCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);	
+  		
+  	}
+
+  	public CachedRowSet getSubjectCount(){
+ 	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM contentindicator WHERE "
+  							+"contentindicator LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getSubjectCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);		
+  		
+  	}
+
+  	public CachedRowSet getResourceCount(){
+ 	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM item WHERE "
+  							+"title LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getResourceCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);	
+  		
+  	}
+  	
+  	public CachedRowSet getWorksCount(){
+ 	  	StringBuffer m_sql_string = new StringBuffer("");
+  	    Statement l_stmt;
+  	    CachedRowSet l_crset = null;
+  	    
+  		
+  		m_sql_string.append("SELECT count(*) "
+  							+"FROM work WHERE "
+  							+"work_title LIKE '"+m_key_word+"'");
+  	    
+  		try {
+  	      l_stmt = m_db.m_conn.createStatement();      
+  	      l_crset =  m_db.runSQL(new String(m_sql_string.toString().trim()), l_stmt);
+  	      l_stmt.close();
+  	     // System.out.println("SQL STRING: " + m_sql_string.toString());
+
+  	    } catch (Exception e) {
+  	      System.out.println(">>>>>>>> EXCEPTION <<<<<<<<");
+  	      System.out.println("An Exception occured in SearchCount.getWorksCount()");
+  	      System.out.println("MESSAGE: " + e.getMessage());
+  	      System.out.println("LOCALIZED MESSAGE: " + e.getLocalizedMessage());
+  	      System.out.println("SQL STRING: " + m_sql_string.toString());
+  	      System.out.println(">>>>>>>>>>>>>-<<<<<<<<<<<<<");
+  	    }
+  	    
+  	    return (l_crset);	
+  		
+  	}
+
+
+  	
+  	/*******************
+    SET FUNCTIONS
+    *******************/
+  	public void setKeyWord(String p_key_word) {
+  		m_key_word = p_key_word.trim();
+  		m_key_word = m_key_word.replaceAll(' ','% %');
+  		m_key_word = '%'+m_key_word.toLowerCase()+'%';
+  		System.out.println("search term is "+m_key_word);
+  	}
+  }
+
+ 
